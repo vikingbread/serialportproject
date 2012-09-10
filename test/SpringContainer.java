fe8b5b86bb4d5c62fe312c18ef7fc60e732f7632
vikingbread <breadviking@gmail.com>
2012-09-10 14:14:00 +0800

    commit test src

---
+++ test/SpringContainer.java
@@ -0,0 +1,16 @@
+package com.njit.linker.test;
+
+import org.junit.Test;
+
+import com.njit.linker.util.WebUtil;
+
+public class SpringContainer {
+
+	@Test
+	public void getBean(){
+//		ClassPathXmlApplicationContext	classPathXmlApplicationContext = new ClassPathXmlApplicationContext(
+//		"classpath:/beans.xml");
+//		WebUtil webUtil = (WebUtil) classPathXmlApplicationContext.getBean("webUtil");
+//		Assert.notNull(webUtil);
+	}
+}
